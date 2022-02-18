874ed0d6c06ac89678c76cd7b150b960accf729e
Yoanna Yotova <yyotova@asteasolutions.com>
2022-02-18 22:42:39 +0200

    fix index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,10 +4,12 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store, persistor } from "./store";
 import { PersistGate } from 'redux-persist/integration/react'
- 
+
 ReactDOM.render(
-    <Provider store={store}>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </Provider>,
+    </PersistGate>
+  </Provider>,
   document.getElementById("root")
 );
